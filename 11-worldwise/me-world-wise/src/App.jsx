6a4cd6bca0255f637ad7afa51bf2cb2059537eb2
ali <aliyoo7887@gmail.com>
2023-08-05 17:28:11 +0330

    create and modified some components for app

--- 11-worldwise/me-world-wise/src/App.jsx
+++ 11-worldwise/me-world-wise/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
         <Route path='/' element={<Homepage />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
+        <Route path='login' element={<Login />} />
+        {/* <Route path='app' element={<AppLayout />} /> */}
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
