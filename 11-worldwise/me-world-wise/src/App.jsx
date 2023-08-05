3eb71bdb684261aca32c59a11dc9da02391d2754
ali <aliyoo7887@gmail.com>
2023-08-05 18:44:29 +0330

    create app page and appNav for navigation

--- 11-worldwise/me-world-wise/src/App.jsx
+++ 11-worldwise/me-world-wise/src/App.jsx
@@ -4,16 +4,22 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
         <Route path='login' element={<Login />} />
-        {/* <Route path='app' element={<AppLayout />} /> */}
+        <Route path='app' element={<AppLayout />}>
+          {/* <Route index element={<p>cities</p>} /> */}
+          <Route path='cities' element={<p>cities</p>} />
+          <Route path='countries' element={<p>countries</p>} />
+          <Route path='form' element={<p>form</p>} />
+        </Route>
         <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
